5b3ced3ee8e161a78b87fda8d108878b4f4e6e64
prasanna <proplayer14102004@gmail.com>
2026-02-19 11:27:04 +0530

    feat: implement user registration and login with JWT authentication

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const Datastore = require('nedb-promises')//in memory database for Node.js
-const bcrypt = require('bcryptjs')
+const Datastore = require("nedb-promises"); //in memory database for Node.js
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken')
+
+const config = require('./config')
+
 const app = express();
 
 app.use(express.json());
 
-const users = Datastore.create('Users.db')
+const users = Datastore.create("Users.db");
 
 app.get("/", (req, res) => {
   res.send("REST API with Authentication");
@@ -15,24 +19,67 @@ app.post("/api/auth/register", async (request, response) => {
   try {
     const { name, email, password } = request.body;
     if (!name || !email || !password) {
-      return response.status(422).json({ //Missing required fields
+      return response.status(422).json({
+        //Missing required fields
+        message: "Please Fill all details",
+      });
+    }
+    if (await users.findOne({ email })) {
+      return response.status(409).json({
+        message: " email already exist",
+      });
+    }
+    const hashedpassword = await bcrypt.hash(password, 10); // salt is 10
+    const newUser = await users.insert({
+      name,
+      email,
+      password: hashedpassword,
+    });
+    return response.status(201).json({
+      message: "User registered successfully",
+      id: newUser._id,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message,
+    });
+  }
+});
+app.post("/api/auth/login", async (request, response) => {
+  try {
+    const { email, password } = request.body;
+    if (!email || !password) {
+      return response.status(422).json({
+        //Missing required fields
         message: "Please Fill all details",
       })}
-      if(await users.findOne({email})){
-        return response.status(409).json({
-            message: " email already exist"
+      const user = await users.findOne({email})
+      if(!user){
+        return res.status(401).json({
+            message: 'Email or Password is Invalid'
         })
       }
-      const hashedpassword = await bcrypt.hash(password,10)// salt is 10
-      const newUser = await users.insert({
-        name,
-        email,
-        password: hashedpassword
-      })
-      return response.status(201).json({
-        message: 'User registered successfully',
-        id: newUser._id
-      })
+      const passwordMatch= await bcrypt.compare(password, user.password)
+      if(!passwordMatch){
+         return res.status(401).json({
+            message: 'Email or Password is Invalid'
+        })
+      }
+  const accessToken = jwt.sign(
+    {userId: user._id},
+    config.accessTokenSecret,
+    {subject: 'accessApi', expiresIn: '1h'}
+) 
+  return response.status(200).json({
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    accessToken
+  })
+
+
+  
+
   } catch (error) {
     return response.status(500).json({
       message: error.message,
