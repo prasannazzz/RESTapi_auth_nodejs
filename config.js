c63d5e96979fc81bb78692085fc743d01feca423
prasanna <proplayer14102004@gmail.com>
2026-02-20 08:29:17 +0530

    feat: add 2FA support with temporary token caching and QR code generation
    - Updated config.js to include cache settings for temporary tokens.
    - Enhanced login flow in index.js to handle 2FA with temporary tokens.
    - Implemented 2FA generation and validation endpoints.
    - Added necessary dependencies for OTP generation and QR code creation.

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 module.exports = {
     accessTokenSecret: 'myAccessTokenSecret',
     accessTokenExpiresIn: '30m',
+
     refreshTokenSecret: 'myRefreshTokenSecret',
-    refreshTokenExpiresIn: '1w'
+    refreshTokenExpiresIn: '1w',
+
+    cacheTemporaryTokenPrefix: 'tempToken:',
+    cacheTemporaryTokenExpiresInSeconds: 180 // 5 minutes in seconds
 }
--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const express = require("express");
 const Datastore = require("nedb-promises"); //in memory database for Node.js
 const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken')
-
+const jwt = require('jsonwebtoken');
+const { generateSecret , generateURI } = require("otplib");
+const qrcode = require('qrcode')
 const config = require('./config')
+const crypto = require('crypto')
+const NodeCache = require('node-cache');
 
 const app = express();
 
 app.use(express.json());
 
+const cache = new NodeCache();
+
 const users = Datastore.create("Users.db");
 const userRefreshTokens = Datastore.create("UserRefreshTokens.db");
 const userInvalidTokens = Datastore.create("UserInvalidTokens.db");
 
-app.get("/", (request, response) => {
+app.get('/', (request, response) => {
   response.send("REST API with Authentication");
 });
 
-app.post("/api/auth/register", async (request, response) => {
+app.post('/api/auth/register', async (request, response) => {
   try {
     const { name, email, password , role} = request.body;
     if (!name || !email || !password) {
@@ -28,7 +33,7 @@ app.post("/api/auth/register", async (request, response) => {
     }
     if (await users.findOne({ email })) {
       return response.status(409).json({
-        message: " email already exist",
+        message: " Email already exist",
       });
     }
     const hashedpassword = await bcrypt.hash(password, 10); // salt is 10
@@ -36,7 +41,9 @@ app.post("/api/auth/register", async (request, response) => {
       name,
       email,
       password: hashedpassword,
-      role: role ?? 'member'
+      role: role ?? 'member',
+      '2faEnable': false ,
+      '2faSecret': null
     });
     return response.status(201).json({
       message: "User registered successfully",
@@ -49,7 +56,7 @@ app.post("/api/auth/register", async (request, response) => {
   }
 });
 
-app.post("/api/auth/login", async (request, response) => {
+app.post('/api/auth/login', async (request, response) => {
   try {
     const { email, password } = request.body;
     if (!email || !password) {
@@ -70,18 +77,27 @@ app.post("/api/auth/login", async (request, response) => {
         message: "Email or Password is Invalid",
       });
     }
-    const accessToken = jwt.sign(
+    if(user['2faEnable']){
+        const tempToken = crypto.randomUUID()
+
+        cache.set(config.cacheTemporaryTokenPrefix + tempToken, user._id, config.cacheTemporaryTokenExpiresInSeconds) // expire in 5 minutes
+
+        return response.status(200).json({
+            tempToken,
+            expireIn: config.cacheTemporaryTokenExpiresInSeconds
+        })
+    }else{
+      const accessToken = jwt.sign(
       { userId: user._id },
       config.accessTokenSecret,
       { subject: "accessApi", expiresIn: config.accessTokenExpiresIn },
     );
-
-    const refreshToken = jwt.sign(
+      const refreshToken = jwt.sign(
       { userId: user._id },
       config.refreshTokenSecret,
       { subject: "refreshToken", expiresIn: config.refreshTokenExpiresIn },
     );
-    await userRefreshTokens.insert({
+     await userRefreshTokens.insert({
       userId: user._id,
       refreshToken
     })
@@ -92,12 +108,51 @@ app.post("/api/auth/login", async (request, response) => {
       accessToken,
       refreshToken
     });
+    } 
   } catch (error) {
     return response.status(500).json({
       message: error.message,
     });
   }
 });
+
+app.post('/api/auth/login/2fa', async(request, response) => {
+    try {
+      const { tempToken, totp } = request.body
+      if(!tempToken || !totp){
+        return response.status(422).json({message: 'Please fill in all fields(tempToken and totp)'})
+      }
+      const userId = cache.get(config.cacheTemporaryTokenPrefix + tempToken)
+      if(!userId){
+        return response.status(401).json({message: 'The provided tempToken is invalid or expired'})
+      }
+      const user = await users.findOne({_id: userId})
+      const verified = authenticator.check({totp, secret: user['2faSecret']})
+      if(!verified){
+        return response.status(401).json({message: 'Invalid TOTP code'})
+      }
+      const accessToken = jwt.sign(
+        { userId: user._id },
+        config.accessTokenSecret,
+        { subject: "accessApi", expiresIn: config.accessTokenExpiresIn },
+      );
+      const refreshToken = jwt.sign(
+        { userId: user._id },
+        config.refreshTokenSecret,
+        { subject: "refreshToken", expiresIn: config.refreshTokenExpiresIn },
+      );
+       await userRefreshTokens.insert({
+        userId: user._id,
+        refreshToken
+      })
+      return response.status(200).json({
+        accessToken,
+        refreshToken
+      })
+    } catch (error) {
+        return response.status(500).json({message: error.message})
+    }
+})
 
 app.post('/api/auth/refresh-token', async (request, response) => {
     try {
@@ -140,7 +195,75 @@ app.post('/api/auth/refresh-token', async (request, response) => {
     }
 })
 
-app.get('/api/auth/logout', ensureAuthenticated, async (request, response) => {
+// app.get('/api/auth/2fa/generate', ensureAuthenticated, async(request,response)=>{
+//     try {
+//        const user =await users.findOne({_id: request.user.id})
+//        const secret = authenticator.generateSecret()
+//        const uri = authenticator.keyuri(user.email, 'MyApp', secret)
+
+//        await users.update({_id: request.user.id},{$set:{'2faSecret': secret}})
+//        await users.compactDatafile()
+
+//        const qrCode = await qrcode.toBuffer(uri, {type: 'image/png', margin: 1})
+
+//        response.setHeader('Content-Disposition', 'attachment; filename=qrcode.png')
+//        return response.status(200).type('image/png').send(qrCode)
+
+//     } catch (error) {
+//        return response.status(500).json({message:error.message}) 
+//     }
+// })
+
+app.get('/api/auth/2fa/generate',ensureAuthenticated, async (request, response) => {
+  try {
+    const user = await users.findOne({ _id: request.user.id });
+    if (!user) {
+      return response.status(404).json({ message: "User not found" });
+    }
+    // Generate Base32 secret
+    const secret = generateSecret();
+    // Generate otpauth URI for QR code
+    const uri = generateURI({
+      issuer: "MyApp",
+      label: user.email,
+      secret,
+    });
+    // Save secret in DB
+    await users.update(
+      { _id: request.user.id },
+      { $set: { "2faSecret": secret } }
+    );
+    // Create QR image buffer
+    const qrCode = await qrcode.toBuffer(uri, { type: "image/png", margin: 1 });
+
+    response.setHeader("Content-Disposition", "attachment; filename=qrcode.png");
+    return response.status(200).type("image/png").send(qrCode);
+
+  } catch (error) {
+    return response.status(500).json({ message: error.message });
+  }
+});
+
+app.post('/api/auth/2fa/validate',ensureAuthenticated, async(request, response) => {
+    try {
+        const {totp} = request.body
+        if(!totp){
+            return response.status(422).json({message: 'TOTP code is required'})
+        }
+        const user = await users.findOne({_id: request.user.id})
+        const verified = authenticator.verify({token: totp, secret: user['2faSecret']})
+        if(!verified){
+            return response.status(400).json({message: 'totp is not correct or expired'})
+        }
+        await users.update({_id: request.user.id},{$set:{'2faEnable': true}})
+        users.compactDatafile()
+        return response.status(200).json({message: '2FA has been enabled successfully'})
+    } catch (error) {
+        return response.status(500).json({ message: error.message });
+    }
+})
+
+app.get('/api/auth/logout',ensureAuthenticated, async (request, response) => {
     try {
         await userRefreshTokens.remove({userId: request.user.id})
         await userRefreshTokens.compactDatafile()
@@ -156,7 +279,7 @@ app.get('/api/auth/logout', ensureAuthenticated, async (request, response) => {
     }
 })
 
-app.get('/api/users/current', ensureAuthenticated,async(request, response) => {
+app.get('/api/users/current',ensureAuthenticated, async(request, response) => {
    try {
     const user = await users.findOne({_id: request.user.id})
     return response.status(200).json({
@@ -171,15 +294,15 @@ app.get('/api/users/current', ensureAuthenticated,async(request, response) => {
    }
 });
 
-app.get('/api/admin',ensureAuthenticated, authorize(['admin']),(request,response) => {
+app.get('/api/admin',ensureAuthenticated, authorize( ['admin'] ),(request, response) => {
     return response.status(200).json({message: 'Only admins can access this route'})
 });
 
-app.get('/api/moderator',ensureAuthenticated, authorize(['admin', 'moderator']),(request,response) => {
+app.get('/api/moderator',ensureAuthenticated, authorize( ['admin', 'moderator'] ),(request, response) => {
   return response.status(200).json({message: 'Only admins and moderator can access this route'})
 });
 
-async function ensureAuthenticated(request,response,next){
+async function ensureAuthenticated(request, response, next){
     const accessToken = request.headers.authorization
     if(!accessToken){
         return response.status(401).json({
@@ -225,4 +348,4 @@ function authorize(roles=[]){
     }
 }
 
-app.listen(3003, () => console.log("Server is running on port 3003"));
+app.listen(3006, () => console.log("Server is running on port 3006"));
